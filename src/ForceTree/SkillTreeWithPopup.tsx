2bb604911f6afb60ab3b591a5324764711a217bc
GrimbeardHalt <xiaofx@umich.edu>
2026-02-28 17:06:39 -0500

    tiny change

--- src/ForceTree/SkillTreeWithPopup.tsx
+++ src/ForceTree/SkillTreeWithPopup.tsx
@@ -72,7 +72,7 @@ export default function SkillTreeWithPopup({ data, onNodeClick }: Props) {
 
   return (
     <div ref={containerRef} className="relative w-full h-full">
-      <div className="absolute top-1/2 left-6 -translate-y-1/2 z-20 pointer-events-none">
+      <div className="hidden lg:flex absolute top-1/2 left-6 -translate-y-1/2 z-20 pointer-events-none ">
         <ul className="list-disc pl-6 text-sm md:text-lg rounded-lg p-3">
           <li><strong>Click</strong> a circle to explore an experience</li>
           <li><strong>Larger</strong> circles represent <strong>higher impact</strong> work</li>
